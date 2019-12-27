58fdc64559f8a499492b3850afe6e31b32957944
liantan <liantan@cisco.com>
2019-12-27 10:27:12 +0800

    add css

---
+++ src/konstellate-editor/kind.pick.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './kind.pick.css';
+
+class KindPicker extends React.Component {
+    render() {
+        return (
+            <div className='kind-picker-container'>
+                <h1>I'm KindPicker!</h1>
+            </div>
+        )
+    }
+}
+
+export default KindPicker;
---
+++ src/konstellate-editor/monaco.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './monaco.css';
+
+class Monaco extends React.Component {
+    render() {
+        return (
+            <div className='monaco-container'>
+                <h1>I'm Monaco</h1>
+            </div>
+        );
+    }
+}
+
+export default Monaco;
