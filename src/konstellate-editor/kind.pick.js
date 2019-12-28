e5281ed6441bb79e45fa8f9335e3d94732a8c633
isaactl <yefeng1223@qq.com>
2019-12-28 22:08:15 +0800

    git full json schema of k8s resource

--- src/konstellate-editor/kind.pick.js
+++ src/konstellate-editor/kind.pick.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import './kind.pick.css';
+import axios from 'axios'
+import SwaggerParser from 'swagger-parser';
 
 class KindPicker extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: null
+        }
+    }
+    loadData = () => {
+        var api = 'https://raw.githubusercontent.com/kubernetes/kubernetes/master/api/openapi-spec/swagger.json';
+        axios.get(api).then((response) => {
+            let d = response.data.definitions
+            SwaggerParser.validate(response.data, (err, api) => {
+                if (err) {
+                    console.error(err);
+                }
+                else {
+                    console.log("API name: %s, Version: %s", api.info.title, api.info.version);
+                    // console.log(api.definitions["io.k8s.api.apps.v1.Deployment"])
+
+                    console.log(JSON.stringify(api.definitions["io.k8s.api.apps.v1.Deployment"]))
+                }
+            });
+
+            this.setState({ data: d })
+        }).catch(function (err) {
+            console.log(err)
+        })
+
+
+    }
     render() {
+        if (this.state.data == null) {
+            this.loadData()
+        }
         return (
             <div className='kind-picker-container'>
                 <h1>I'm KindPicker!</h1>
             </div>
+
         )
     }
 }
