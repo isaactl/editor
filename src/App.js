6a179524ede3b1e10b2a8f88c89e129322705eca
isaactl <yefeng1223@qq.com>
2020-01-01 10:57:47 +0800

    fix css issue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Editor from './konstellate-editor/editor';
+// import Editor from './konstellate-editor/editor';
+import Editor from './monaco-editor/editor';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>What to show?</h1>
       <Editor />
     </div>
   );
---
+++ src/monaco-editor/editor.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import MonacoEditor from 'react-monaco-editor';
+
+class Monaco extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            code: 'kind: Deployment\nname: hello world',
+        }
+    }
+    editorDidMount(editor, monaco) {
+        console.log('editorDidMount', editor);
+        editor.focus();
+    }
+    onChange(newValue, e) {
+        console.log('onChange', newValue, e);
+    }
+    render() {
+        const code = this.state.code;
+        const options = {
+            selectOnLineNumbers: true
+        };
+        return (
+            <MonacoEditor
+                width="800"
+                height="600"
+                language="yaml"
+                theme="vs-dark"
+                value={code}
+                options={options}
+                onChange={this.onChange}
+                editorDidMount={this.editorDidMount}
+            />
+        );
+    }
+}
+
+export default Monaco;
