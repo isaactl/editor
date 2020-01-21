0c952ddd8a315e7e44aeca30a66d5d672659cd80
liantan <liantan@cisco.com>
2020-01-21 18:20:41 +0800

    use anyOf to support valueFrom

--- src/customized/env.js
+++ src/customized/env.js
@@ -1,9 +1,87 @@
 import React from 'react';
+import Env from './env.json';
+import Form from "react-jsonschema-form";
 
 export default class extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            type: props.type,
+            schema: null,
+            keyList: [],
+            nameList: [],
+            selectedKey: '',
+            formData: {},
+        };
+
+        this._loadSwagger=this._loadSwagger.bind(this);
+    }
+
+    componentDidMount() {
+        // this._loadSwagger()
+    }
+
+    _loadSwagger() {
+
+    }
+
+    CustomFieldTemplate(props) {
+        const {id, classNames, label, help, required, description, errors, children} = props;
         return (
-            <div>use anyof https://rjsf-team.github.io/react-jsonschema-form/</div>
+            <div className={classNames}>
+                <label htmlFor={id+label}>{label}{required ? "*" : null}</label>
+                {help}
+                {children}
+                {errors}
+                {description}
+            </div>
+        );
+    }
+
+    // https://jsfiddle.net/69z2wepo/68259/
+    handleChange(data) {
+        const formData = JSON.parse(JSON.stringify(data.formData));
+        // console.log(formData);
+        if (this.state.selectedKey !== formData.key) {
+            formData.name = "";
+            // assign name list, TODO: load from k8s
+            const nameList = [
+                formData.key + "_v1",
+                formData.key + "isekf"
+            ];
+            // ValueFrom.properties.name = Object.assign(ValueFrom.properties.name, {
+            //     "enum": nameList,
+            // });
+
+            this.setState({
+                selectedKey: formData.key,
+                nameList: nameList,
+                formData: formData,
+            });
+        } else {
+            this.setState({
+                formData: formData
+            })
+        }
+    }
+
+    onSubmit(data) {
+        console.log(data.formData)
+    }
+
+    render() {
+        // if (this.state.schema == null) {
+        //     return <div>Nothing to show</div>
+        // }
+
+        return (
+            <Form
+                schema={Env}
+                formData={this.state.formData}
+                onSubmit={this.onSubmit.bind(this)}
+                // FieldTemplate={this.CustomFieldTemplate.bind(this)}
+                onChange={this.handleChange.bind(this)}
+            />
         )
     }
 }
--- src/customized/index.js
+++ src/customized/index.js
@@ -1,37 +1,25 @@
 import React from 'react';
 import Swagger from './swagger.json';
-import ValueFrom from './configMapKeyRef.json';
-import Form from "react-jsonschema-form";
+import Env from './env';
 import SwaggerParser from 'swagger-parser';
 
+// load all schemas of kubernetes v1.14.9
 export default class extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            schema: null,
-            keyList: [],
-            nameList: [],
-            selectedKey: '',
-            formData: {},
+            schemas: null,
         };
 
-        this._loadResources=this._loadResources.bind(this);
+        this._loadSwagger=this._loadSwagger.bind(this);
     }
 
     componentDidMount() {
-        ValueFrom.properties.key = Object.assign(ValueFrom.properties.key, {
-            "enum": [
-                "soe",
-                "oiieklw",
-            ],
-        });
-        console.log(ValueFrom);
-        this.setState({schema: ValueFrom})
-        // this._loadResources()
+        this._loadSwagger()
     }
 
-    // load configmap / secret list from k8s cluster
-    _loadResources() {
+    // load config map / secret list from k8s cluster
+    _loadSwagger() {
         SwaggerParser.validate(Swagger, (err, api) => {
             if (err) {
                 console.error(err);
@@ -40,70 +28,16 @@ export default class extends React.Component {
                 console.log("API name: %s, Version: %s", api.info.title, api.info.version);
                 // console.log(api.definitions["io.k8s.api.apps.v1.Deployment"])
                 // console.log(JSON.stringify(api.definitions["io.k8s.api.apps.v1.Deployment"]))
-                let schema = api.definitions["io.k8s.api.apps.v1.Deployment"];
+                // let schema = api.definitions["io.k8s.api.apps.v1.Deployment"];
 
-                this.setState({schema: schema});
+                this.setState({schemas: api.definitions});
             }
         });
     }
 
-    CustomFieldTemplate(props) {
-        const {id, classNames, label, help, required, description, errors, children} = props;
-        return (
-            <div className={classNames}>
-                <label htmlFor={id}>{label}{required ? "*" : null}</label>
-                {}
-                {children}
-                {errors}
-                {description}
-            </div>
-        );
-    }
-
-    // https://jsfiddle.net/69z2wepo/68259/
-    handleChange(data) {
-        const formData = JSON.parse(JSON.stringify(data.formData));
-        // console.log(formData);
-        if (this.state.selectedKey !== formData.key) {
-            formData.name = "";
-            // assign name list, TODO: load from k8s
-            const nameList = [
-                formData.key + "_v1",
-                formData.key + "isekf"
-            ];
-            ValueFrom.properties.name = Object.assign(ValueFrom.properties.name, {
-                "enum": nameList,
-            });
-
-            this.setState({
-                selectedKey: formData.key,
-                nameList: nameList,
-                formData: formData,
-            });
-        } else {
-            this.setState({
-                formData: formData
-            })
-        }
-    }
-
-    onSubmit(data) {
-        console.log(data.formData)
-    }
-
     render() {
-        if (this.state.schema == null) {
-            return <div>Nothing to show</div>
-        }
-
         return (
-            <Form
-                schema={this.state.schema}
-                formData={this.state.formData}
-                onSubmit={this.onSubmit.bind(this)}
-                FieldTemplate={this.CustomFieldTemplate.bind(this)}
-                onChange={this.handleChange.bind(this)}
-            />
+            <Env />
         )
     }
 }
