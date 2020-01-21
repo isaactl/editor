e3864061715f758857a4353a871ac449a34fbcbc
isaactl <yefeng1223@qq.com>
2020-01-21 22:44:00 +0800

    add className assign example

--- src/customized/resource.js
+++ src/customized/resource.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Resource from './resource.json';
 import Form from "react-jsonschema-form";
+import './resource.css';
 
 export default class extends React.Component {
     constructor(props) {
@@ -36,14 +37,50 @@ export default class extends React.Component {
         console.log(JSON.stringify(data.formData))
     }
 
+    customFieldTemplate(props) {
+        const {id, classNames, label, help, required, description, errors, children} = props;
+        return (
+            <div className={classNames}>
+                <label htmlFor={id}>{label}{required ? "*" : null}</label>
+                {description}
+                {children}
+                {errors}
+                {help}
+            </div>
+        );
+    }
+
     render() {
         return (
             <Form
                 schema={this.state.schema}
+                uiSchema={uiSchema}
+                FieldTemplate={this.customFieldTemplate.bind(this)}
                 formData={this.state.formData}
+                fields={fields}
                 onSubmit={this.onSubmit.bind(this)}
                 onChange={this.handleChange.bind(this)}
             />
         )
     }
 }
+
+const uiSchema = {
+  limits: {
+      cpu: {
+        classNames: "tag"
+      },
+      memory: {
+        classNames: "tag"
+      }
+  }
+};
+
+const CustomTitleField = ({title, required}) => {
+    const legend = required ? title + '*' : title;
+    return <div id="custom">{legend}</div>;
+};
+
+const fields = {
+    TitleField: CustomTitleField
+};
